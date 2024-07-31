fde6783dc489bd290f7bf56405e296e381114bed
Manar-El-sayed <manarelsayed542>
2024-07-31 15:20:58 +0300

    add html , css , bootstrap

---
+++ add_task.php
@@ -0,0 +1,95 @@
+<?php
+@include 'db.php';
+
+if (isset($_POST['add_task'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    if (empty($title) || empty($description)) {
+        $message[] = 'Please fill out all fields.';
+    } else {
+        
+            $insert = "INSERT INTO tasks(title, description) VALUES('$title','$description')";
+            $upload = mysqli_query($conn, $insert);
+
+            if ($upload) {
+                $message[] = 'New task added successfully.';
+            } else {
+                $message[] = 'Could not add this task.';
+            }
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>To Do List</title>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+     <link rel="stylesheet" href="list.css">
+ </head>
+ <body class="add-container p-1 text-capitalize">
+     <div class="container mt-4">
+             <div class="title fs-4 fw-medium">Add a task to your to-do-list</div>
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+                <?php
+                if(isset($message)){
+                foreach($message as $message){
+                echo '<span class="error-msg">' .$message. '</span>';
+               }
+              }
+             ?>
+                 <div class="user-details d-flex justify-content-between flex-wrap">
+                         <div class="input-box mb-2">
+                             <span class="details">Task Title</span>
+                             <input type="text" placeholder="Enter the title of task" id="title" name="title" required>
+                         </div>
+                         <div class="input-box">
+                             <span class="details">Description</span>
+                             <input type="text" placeholder="Enter Task Description" id="description" name="description" required>
+                         </div>
+                 </div>
+                 <div class="button my-4">
+                         <input type="submit" name="add_task" value="Add a task to your to-do list">
+                 </div>
+             </form>
+         </div>
+         <?php
+         $select = mysqli_query($conn, "SELECT * FROM tasks");
+          ?>
+              <div class="display my-5 d-flex justify-content-center">
+                 <table class="display-table">
+                     <thead>
+                         <tr>
+                             <th>name</th>
+                             <th>description</th>
+                             <th>create date</th>
+                             <th>update date</th>
+                             <th>action</th>
+                         </tr>
+                     </thead>
+                     <?php
+                     while($row = mysqli_fetch_assoc($select)){
+                         ?>
+                        <tr>
+                           <td><?php echo $row['title'];?></td>
+                           <td><?php echo $row['description'];?></td>
+                           <td><?php echo $row['createdAt'];?></td>
+                           <td><?php echo $row['updatedAt'];?></td>
+                           <td>
+                               <a href="read.php?edit=<?php echo $row['id'];?>" class="btn"><i class="fa-solid fa-eye"></i>   view</a>
+                               <a href="update.php?edit=<?php echo $row['id'];?>" class="btn mt-0"> <i class="fas fa-edit"></i>  edit</a>
+                               <a href="add_task.php?delete=<?php echo $row['id'];?>" class="btn mb-0"> <i class="fas fa-trash"></i>  delete</a>
+                           </td>
+                       </tr>
+                       <?php };?>
+               </table>
+              </div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+ </body>
+</html>
--- update.php
+++ update.php
@@ -1,13 +1,17 @@
 <?php
 include'db.php';
-$id = $_GET['id_edit'];
+$id = $_GET['edit'];
 if (isset($_POST['update'])) {
   $title = $_POST['title'];
   $description =$_POST['description'];
   $upd = "UPDATE `tasks` SET title='$title' , description='$description' WHERE id='$id'";
   $result = mysqli_query($conn , $upd );
-  header('location:index.php');
-  mysqli_close($conn);
+  if ($result) {
+    $message[] = 'task updated successfully.';
+  } else {
+    $message[] = 'Could not update this task.';
+  }
+   header('location:add_task.php');
 }
 ?>
 <?php 
@@ -16,41 +20,48 @@ while ($row =mysqli_fetch_assoc($read)) {
   $title =$row['title'];
   $description =$row['description'];
 }
-?>
 
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-     rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous">
-    <title>Document</title>
+    <title>update task</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="list.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body class=" bg-secondary">
-  <div class="container">
-    <div class="title" >
-       <h3 class="text-center text-white fst-italic mt-3 mb-5"> Edit Task</h3>
-    </div>
-    <div class="">
-     <form method="post" action="" class="mb-5 ps-5 rounded-pill border border-whiteborder border-2" >
-      <div class="form-group  d-flex justify-content-center mt-5 mb-4 ">
-        <label for="title"class="fs-5 text text-white me-2" >title:</label>
-        <input type="text" class="form-control w-75 ms-2 " id="title" placeholder="enter title" name="title" value="<?php echo $title ; ?>" require/>
-      </div>
-     <div class="form-group d-flex justify-content-center  ">
-       <label for="description" class="fs-5 text  text-white me-1 ">Description:</label>
-       <input class="form-control w-75 me-5"  name="description" placeholder="enter task" id="description" value="<?php echo $description;?>" require />
-     </div>
-     <div class="button text-center">
-      <button type="submit" name="update" onclick='return confirm("Do you really want to update task")' class=" btn btn-outline-primary my-5  border border-white border border-2 rounded-pill text-white">update</button>
-     </div>
-     
-     </form>
-    </div>
-   
-  </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body class="text-capitalize">
+<div class="update-con centered d-flex justify-content-between align-items-center">
+    <div class="container">
+            <div class="title fs-4 fw-medium">Update task</div>
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+            <?php
+               if(isset($message)){
+               foreach($message as $message){
+               echo '<span class="error-msg">' .$message. '</span>';
+              }
+             }
+            ?>
+            <div class="user-details d-flex flex-column">
+                        <div class="input-box">
+                             <span class="details">Task title</span>
+                             <input type="text" placeholder="Enter the title of task" id="taskName" value="<?php echo $title ; ?>" name="title" required>
+                         </div>
+                         <div class="input-box">
+                             <span class="details">Description</span>
+                             <input type="text" placeholder="Enter Task Description" id="description" value="<?php echo $description;?>" name="description" required>
+                         </div>
+            </div>
+            <div class="button">
+                        <input type="submit" class="btn" name="update" value="update task">
+                        <a class="d-flex justify-content-center mt-3"href="add_task.php">go back</a>
+            </div>
+        </form>
+
+        </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
