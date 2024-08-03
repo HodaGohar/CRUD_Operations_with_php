9a3789d6f038b65219a7a932e8ff8b9e42ae2b22
Nada2998 <nassernada344@gmail.com>
2024-08-03 13:21:22 +0300

    Update & Delete

--- delete.php
+++ delete.php
@@ -1,9 +1,8 @@
 <?php
 include 'db.php';
 $id = $_GET['delete'];
-$del="DELETE FROM `tasks` WHERE id='$id'";
-mysqli_query($conn ,$del );
+$dele="DELETE FROM `tasks` WHERE id='$id'";
+mysqli_query($conn ,$dele );
 header('location:add_task.php');
 mysqli_close($conn);
-
 ?>
--- update.php
+++ update.php
@@ -23,7 +23,7 @@ while ($row =mysqli_fetch_assoc($read)) {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
